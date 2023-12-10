5c911e292351744f60bcef5d20941c220ccf476a
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-12-10 01:34:47 +0000

    com if

--- main.c
+++ main.c
@@ -2,5 +2,14 @@
 
 int main(void) {
   printf("Gott mit uns\n");
+
+  int i = 0;
+  ///Chegará aqui por procedures. é o automato
+
+  if (i == 1) {
+    printf("Gott mit uns\n");
+  } else {
+    printf("stand united with %d", i);
+  }
   return 0;
 }
