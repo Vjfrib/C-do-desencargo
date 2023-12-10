e9822a0a0ced61a6e9adac936360450d32b843e5
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-12-10 01:38:16 +0000

    a gente nunca esquece

--- main.c
+++ main.c
@@ -3,13 +3,20 @@
 int main(void) {
   printf("Gott mit uns\n");
 
-  int i = 0;
+  int identidade = 0;
   ///Chegará aqui por procedures. é o automato
 
-  if (i == 1) {
+  if (identidade == 1) {
     printf("Gott mit uns\n");
   } else {
-    printf("stand united with %d", i);
+    
+    printf("stand united with %d\n\n", identidade);
+
+    int iterador = 13, bound = 10;
+    
+    for (iterador = 2; iterador < bound; iterador++) {
+      printf("\t\tstand united with %d\n", iterador);
+    }
   }
   return 0;
 }
